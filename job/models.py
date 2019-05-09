4d8c557b04182e4afd7a844a250f477c68cc2ecd
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-09 13:11:56 +0900

    add Recruit model

--- job/models.py
+++ job/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Recruit(models.Model):
+    company_name = models.CharField(max_length=50)
+    recruiter_name = models.CharField(max_length=50)
+    job_content = models.TextField()
+    publish_data = models.DateField(auto_now=True, auto_now_add=True)
